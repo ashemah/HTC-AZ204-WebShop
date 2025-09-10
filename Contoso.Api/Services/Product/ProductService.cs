52fa692180fd218d0d928b7febf9e446fd4a888d
Blair Strang <blair.strang@gmail.com>
2025-09-10 12:27:09 +1000

    needful

--- Contoso.Api/Services/Product/ProductService.cs
+++ Contoso.Api/Services/Product/ProductService.cs
@@ -65,7 +65,13 @@ public class ProductsService : IProductsService
             long unixTime = ((DateTimeOffset)currentTime).ToUnixTimeSeconds();
             long releaseDateUnix = long.Parse(releaseDate);
 
-            var imageUri = blobClient.GenerateSasUri(Azure.Storage.Sas.BlobSasPermissions.Read, DateTimeOffset.UtcNow.AddHours(1));
+        var imageUri = "https://t03storage.blob.core.windows.net/t03container/comingsoon.png?sp=r&st=2025-09-10T01:27:43Z&se=2032-06-23T09:42:43Z&spr=https&sv=2024-11-04&sr=b&sig=fSE6dU5VPOb3fo%2FTd2bYvH5QClnAQeYrNeGrbZwgv7k%3D";
+        try
+        {
+            imageUri = blobClient.GenerateSasUri(Azure.Storage.Sas.BlobSasPermissions.Read, DateTimeOffset.UtcNow.AddHours(1)).ToString();
+        } catch {}
+
+
             if (unixTime >= releaseDateUnix)
             {
                 //item.ImageUrl = item.ImageUrl + "?sp=r&st=2025-09-10T00:11:53Z&se=2025-09-10T08:26:53Z&spr=https&sv=2024-11-04&sr=c&sig=DmFnQeB9yO%2FKaiHNrZzRXL1ATszt0t0opG3uI0UArZw%3D";
